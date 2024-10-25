e66d46bb875555dd4305cfa182ca4b955106bc59
Mert Karayakuplu <mertkarayakuplu@gmail.com>
2024-10-25 18:12:21 +0300

    add opts
    Signed-off-by: Mert Karayakuplu <mertkarayakuplu@gmail.com>

--- init.lua
+++ init.lua
@@ -1,3 +1,147 @@
+-- Sources / Inspirations / Examples / Or just good stuff in general,
+-- https://github.com/tralph3/.dotfiles/blob/master/configs/neovim.org
+
+-- Options
+vim.opt.allowrevins = false -- reverse insert mode
+vim.opt.autochdir = false -- auto change working dir
+vim.opt.autoindent = true -- auto indent
+vim.opt.autoread = true -- auto read when a file is changed outside
+-- no longer supported or new?
+-- vim.opt.autorewrite = false
+-- auto write files after some commands
+-- vim.opt.autorewriteall = false
+-- and this is for the rest of the commands
+-- this includes stuff like :quit
+vim.opt.background = "dark" -- dark or light, to guide themes
+vim.opt.backspace = "indent,eol,start" -- also possible, nostop
+vim.opt.backup = true -- use backups
+vim.opt.backupcopy = "yes" -- overwrite original, new file as backup
+-- vim.opt.breakat ... -- line break characters for when linebreak true
+-- vim.opt.linebreak <- and this is the opt previous one affects
+
+-- unnamedplus aliases unnamed register to +, which is the system clipboard
+-- unnamed is the same intent basically, which is *, which seems to effectively mean the same thing on my system
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cmdheight = 1 -- command line height
+
+-- add 32 for the classic column
+vim.opt.colorcolumn = { 72, 80 } -- colors the exact column, 80 is subls 79
+vim.opt.completeopt = { "menuone", "noselect" } -- show menu for completion at all times
+vim.opt.conceallevel = 0 -- dont hide anything
+vim.opt.confirm = true -- ask me if i want to discard unsaved changes
+-- copyindent isnt used, might need later
+vim.opt.cursorcolumn = false -- highlight column
+vim.opt.cursorline = false -- highlight row
+-- opt.emoji defaults to on, might want false later on
+vim.opt.endofline = true -- write eol
+-- equalways to set window resizing, defaults to true
+vim.opt.expandtab = true -- just makes sense
+-- maybe later ... https://vi.stackexchange.com/questions/2970/elastic-tabstops-in-vim
+vim.opt.fileencoding = "utf-8"
+vim.opt.fileformat = "unix" -- for line endings and stuff, dont use windows
+vim.opt.fileformats = "unix,dos" -- likewise consider all files unix first
+-- fillchars is useful for status bar
+vim.opt.fixendofline = true -- fix eols
+-- fold options might be useful later on
+-- fsync might be useful later on during networking
+vim.opt.fsync = true
+-- grepprg to set grep binary
+-- guicursor can be useful later on
+-- vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+
+-- guifontwide to set a different font for wide characters
+-- also skipping guioptions
+-- see also guitab...
+vim.opt.history = 10000 -- 10k is default, lets keep it that way
+vim.opt.hlsearch = true -- highlight all previous matches
+vim.opt.ignorecase = true -- ignore case when doing search
+vim.opt.incsearch = true -- search as you type
+vim.opt.lazyredraw = false -- redraw durin gmacros when false
+-- matchpairs might be useful to modify
+-- matchtime
+-- maxfuncdept is useful to avoid recursion loops, default is 100
+-- maxmapdept is similar
+-- max menu items, default is 25
+vim.opt.menuitems = 50
+-- mkspellmem
+vim.opt.mouse = "a" -- support mouse in all modes
+--mousefocus to auto switch window focus
+--mousehide to hide mouse when typing
+-- modify mousemodel to show a pop up on rightclick
+-- mousescroll sets the shift per scroll
+-- mouseshape sets the cursor shape
+vim.opt.number = true -- show line numbers
+vim.opt.relativenumber = true
+-- not doing relativenumber
+vim.opt.numberwidth = 4 -- width of number column
+-- omnifunc for omni completion
+-- paste is obsolete, pastetoggle can be used if we want to use it anyway
+vim.opt.pumblend = 0 -- pop up menu opacity, 0-100
+vim.opt.pumheight = 10 -- pop up menu height
+vim.opt.pumwidth = 25 -- pop up menu width
+-- redrawdebug to see redraws
+-- redrawtime to see redraw timing
+vim.opt.ruler = true -- line col position in bottom right
+-- rulerformat default %15(%c%V\ %p%%%)
+-- :set rulerformat=%l,%c%V%=%P
+--                   │  │ │ │ └ position in the file in percent (or ALL/TOP/BOT)
+--                   │  │ │ └—— right align the following items
+--                   │  │ └———— virtual (or screen) column, matters for TAB and multibyte chars
+--                   │  └—————— cursor column (in bytes)
+--                   └————————— current line
+-- right align everything instead, and hide virtual position
+-- actually lets just display the column number aligned to right instead,
+vim.opt.rulerformat = "%=%c"
+-- scroll to set scroll size per scroll
+-- scrollback can be useful if we need it later on
+vim.opt.scrolljump = 1 -- when we scroll past the bottom, how many more characters should be placed next?
+vim.opt.scrolloff = 50 -- show more lines at the end of a file, useful for visual mode, these lines wont be numbered
+vim.opt.sessionoptions = "buffers" -- mksession behavior
+-- scrollopt can also be useful
+-- sidescroll to set side scroll width per scroll
+vim.opt.showtabline = 2 -- always show tabs
+vim.opt.smartcase = true -- when searching, dont care about cases except when theres at least 1 ucase
+-- smarttab can be useful
+vim.opt.shiftwidth = 4 -- tab width when auto tabbing
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true -- smart indent
+-- more useful split... opts exist
+vim.opt.splitbelow = true -- force all horizontal splits to go below current window
+vim.opt.splitright = true -- force all vertical splits to go to the right of current window
+vim.opt.swapfile = false -- disable using swapfiles, this makes it so its better to work with big files
+vim.opt.tabstop = 4 -- tab size
+-- a bunch of tag commands, seems awfully useful, lets return to that later
+vim.opt.timeoutlen = 3000 -- ms to wait when mapper is in a loop
+vim.opt.undolevels = 1000 -- 1000 is the default, lets keep it that way
+-- allow persistent undos
+vim.opt.undodir = vim.fn.expand("~/.cache/nvim/undodir")
+vim.opt.undofile = true
+vim.opt.updatecount = 100 -- after this many ms, update swapfile
+vim.opt.updatetime = 2000 -- update file if nothing is typed after this much ms
+-- varsofttabstop can be useful
+-- verbose can be useful to debug
+-- softmess can be useful
+-- setting virtualedit to a static value can be useful but not sure if i want to do that
+-- lots of 'wild' prefixes on w, skipping for now
+-- 'win' keys define neovim gui stuff
+vim.opt.winblend = 0 -- pop-up transparency, 0-100, 0 is none
+vim.opt.wrap = false -- dont wrap text
+
+-- not doing these from the vim course,
+-- vim.cmd "set whichwrap+=<,>,[,],h,l"
+-- vim.cmd [[set iskeyword+=-]]
+-- vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+
+-- disable kommentary creating default mappings before plugin exists
+vim.g.kommentary_create_default_mappings = false
+vim.opt.termguicolors = true
+-- vim.o.shell = 'kitty'
+
+-- add " to word seperators
+vim.opt.iskeyword:append('"')
+-- disable hidden buffers, in practice this means no unexpected exits due to closing a buffer
+vim.api.nvim_command("set nohidden")
+
 -- util
 local map = function(mode, lhs, rhs, opts)
     local options = { noremap = true, silent = true }
